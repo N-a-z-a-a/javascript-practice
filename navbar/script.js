3f754a192da30fb85cfcb35ff64f33eeccd8f61d
N-a-z-a-a <jedwin.ezeh@gmail.com>
2023-01-03 13:06:57 +0100

    add scipt for mobile menu

--- navbar/script.js
+++ navbar/script.js
@@ -1,3 +1,4 @@
+// Desktop view
 function dropdown() {
   const dropdownContent = document.getElementById("dropdownContent");
   dropdownContent.classList.toggle("show");
@@ -13,6 +14,37 @@ window.onclick = function (e) {
       const items = dropdowns[i];
       if (items.classList.contains("show")) {
         items.classList.remove("show");
+      }
+    }
+  }
+};
+
+// Mobile View
+const mobileMenu = document.getElementById("mobileMenu");
+mobileMenu.addEventListener("click", onClick);
+
+const mobileDropdown = document.querySelector(".mobile-dropdown");
+
+function onClick() {
+  mobileDropdown.classList.toggle("mobile-show");
+}
+
+const dropdownItems = document.querySelectorAll(".mobile-nav-item");
+for (let item of dropdownItems) {
+  item.addEventListener("click", function () {
+    if (mobileDropdown.classList.contains("mobile-show")) {
+      mobileDropdown.classList.remove("mobile-show");
+    }
+  });
+}
+
+window.onclick = function (e) {
+  if (!e.target.matches(".mobile-menu")) {
+    const mobileDropdown = document.getElementsByClassName("mobile-dropdown");
+    for (let i = 0; i < mobileDropdown.length; i++) {
+      const items = mobileDropdown[i];
+      if (items.classList.contains("mobile-show")) {
+        items.classList.remove("mobile-show");
       }
     }
   }
