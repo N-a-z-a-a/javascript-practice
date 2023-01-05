f28111af432d2ade037c04ba37f9340b6506453c
N-a-z-a-a <jedwin.ezeh@gmail.com>
2023-01-05 10:27:53 +0100

    add modal

---
+++ modal/script.js
@@ -0,0 +1,19 @@
+const modalButton = document.querySelector(".modal-1");
+
+modalButton.addEventListener("click", onClick);
+function onClick() {
+  const modal = document.querySelector(".modal-container");
+  modal.style.display = "flex";
+}
+
+window.onclick = function (e) {
+  if (!e.target.matches(".modal-1") && !e.target.matches(".modal")) {
+    const modal = document.querySelectorAll(".modal-container");
+    for (let i = 0; i < modal.length; i++) {
+      const items = modal[i];
+      if (items.style.display === "flex") {
+        items.style.display = "none";
+      }
+    }
+  }
+};
