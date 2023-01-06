7c0abfd9ef8891d9d126d39306c06214230472fe
N-a-z-a-a <jedwin.ezeh@gmail.com>
2023-01-06 10:30:16 +0100

    add feature to show modal after 5 seconds

--- modal/script.js
+++ modal/script.js
@@ -4,7 +4,10 @@ modalButton.addEventListener("click", onClick);
 function onClick() {
   const modal = document.querySelector(".modal-container");
   modal.style.display = "flex";
+  console.log("worked");
 }
+
+setTimeout(onClick, 5000); //This brings up the modal after a set number of time.
 
 window.onclick = function (e) {
   if (!e.target.matches(".modal-1") && !e.target.matches(".modal")) {
