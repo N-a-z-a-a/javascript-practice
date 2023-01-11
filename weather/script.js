f1d93dfe9cc5773f4d804e07f238e9cd14318f11
N-a-z-a-a <jedwin.ezeh@gmail.com>
2023-01-11 07:38:17 +0100

    update styling and states

--- weather/script.js
+++ weather/script.js
@@ -1,20 +1,8 @@
 const key = "544eb5a9c2e94d84842130842231001";
 const baseURL = "https://api.weatherapi.com/v1";
-// requestURL = `${baseURL}/current.json?key=${key}&q=${lat},${lon}`;
-// "https://api.weatherapi.com/v1/current.json?key=544eb5a9c2e94d84842130842231001&q=Nigeria&aqi=no";
-
-// fetch(requestURL)
-//   .then((response) => response.json())
-//   .then((data) => console.log(data));
-
-// const success = (position) => console.log(position);
-// const error = (error) => console.log(error);
-
-// navigator.geolocation.getCurrentPosition(success, error)
 
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition((success) => {
-    console.log(success.coords);
     const coords = success.coords;
     const lon = coords.longitude;
     const lat = coords.latitude;
@@ -27,7 +15,15 @@ if (navigator.geolocation) {
         if (data) {
           console.log(data);
 
+          const arr = [];
+          arr.push(data);
+          if (arr.length !== 0) {
+            const info = document.querySelector(".message");
+            info.textContent = "";
+          }
+
           const mapIcon = document.querySelector(".map");
+          mapIcon.style.display = "block";
           mapIcon.src = "./assets/mapIcon.jpeg";
           const location = document.querySelector(".place");
           location.textContent = data.location.name;
@@ -36,10 +32,18 @@ if (navigator.geolocation) {
           dateAndTime.textContent = data.location.localtime + "pm";
 
           const icon = document.querySelector(".icon");
+          icon.style.display = "block";
           icon.src = data.current.condition.icon;
 
           const temp = document.querySelector(".value");
           temp.textContent = `${data.current.temp_c}°C`;
+          temp.addEventListener("click", function () {
+            if (temp.textContent.includes("°C")) {
+              temp.textContent = `${data.current.temp_f}°F`;
+            } else {
+              temp.textContent = `${data.current.temp_c}°C`;
+            }
+          });
 
           const condition = document.querySelector(".text");
           condition.textContent = data.current.condition.text;
@@ -50,10 +54,7 @@ if (navigator.geolocation) {
           )}°F`;
 
           const feelsLike = document.querySelector(".feels");
-          feelsLike.textContent = `Feels  like ${data.current.feelslike_c}°C`;
-        } else {
-          const info = document.querySelector(".message");
-          info.textContent = "Please wait while your data is fetching";
+          feelsLike.textContent = `Feels like ${data.current.feelslike_c}°C`;
         }
       });
   });
